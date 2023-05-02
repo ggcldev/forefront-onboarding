361706a7af9373202bbf4aada862362eac8c3ace
ggcldev <ggcldev@gmail.com>
2023-05-03 03:54:45 +0800

    Upload to Github

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.querySelectorAll('.video-thumbnail').forEach(function (thumbnail) {
+// 	thumbnail.addEventListener('click', function () {
+// 		let videoUrl = this.getAttribute('data-url')
+// 		let videoPlayer = document.getElementById('videoPlayer')
+// 		videoPlayer.setAttribute('src', videoUrl)
+// 		$('#videoModal').modal('show')
+// 	})
+// })
+
+// $('#videoModal').on('hidden.bs.modal', function () {
+// 	let videoPlayer = document.getElementById('videoPlayer')
+// 	videoPlayer.pause()
+// 	videoPlayer.currentTime = 0
+// })
